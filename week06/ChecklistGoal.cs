27f694615c8ebbc8b9a36a3d5f5e3011be8d3e31
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-29 20:37:46 -0500

    coded the checklist goal, and the constructor with the parameters

--- week06/ChecklistGoal.cs
+++ week06/ChecklistGoal.cs
@@ -1,1 +1,15 @@
 // Checklist goals, this has to be done multiple times thorugh
+public class ChecklistGoal : Goal
+{
+    private int completedTimes; // this is how many times completed the goal 
+    private int targetTimes; // how many times the goal needs to be completed
+
+    private int bonusPoints; // extra points for completing the goal
+
+
+
+    public ChecklistGoal(string name, string description, int points, int targetTimes, int bonusPoints) : base(name, description, points)
+    {
+
+    }
+}
