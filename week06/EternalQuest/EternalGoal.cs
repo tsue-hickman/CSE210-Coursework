e211f6f90b58da46c8b48168392642125899b18f
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-29 20:23:46 -0500

    coded rest of eternal goal, it did not need much because there is no completion in the program

---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,25 @@
+using System;
+
+// Eternal Goal, never completes, just keep doing it
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points) : base(name, description, points)
+    {
+        // empty
+    }
+
+    public override void RecordEvent()
+    {
+        // does not need complettion, so just needs to add points
+    }
+
+    public override bool IsComplete()
+    {
+        return false; // because it is never complete
+    }
+
+    public override string GtringRepresentation()
+    {
+        return $"EternalGoal:{goalName},{goalDescription},{points}";
+    }
+}
