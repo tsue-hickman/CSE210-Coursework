83c6e9fe602607366254f4cd602e5b451381ee2e
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-29 20:57:35 -0500

    finished coding the overrides to replace methods from base class/ return funcitons

--- week06/ChecklistGoal.cs
+++ week06/ChecklistGoal.cs
@@ -1,4 +1,4 @@
-// Checklist goals, this has to be done multiple times thorugh
+// Checklist goals, this has to be done multiple times thorugh  ~ tracks progress across mulitple completions, calculates when fully done, shows extra bonus info
 public class ChecklistGoal : Goal
 {
     private int completedTimes; // this is how many times completed the goal 
@@ -10,6 +10,29 @@ public class ChecklistGoal : Goal
 
     public ChecklistGoal(string name, string description, int points, int targetTimes, int bonusPoints) : base(name, description, points)
     {
+        completedTimes = 0; //starts at 0
+        this.targetTimes = targetTimes;
+        this.bonusPoints = bonusPoints;
+    }
 
+    public override void RecordEvent()
+    {
+        // will add to  the completedTimes later
+    }
+
+    public override bool IsComplete()
+    {
+        return completedTimes >= targetTimes;
+    }
+
+    //need to show the progress and the bonuses
+    public override string GetDetailsString()
+    {
+        return $"{goalName}: {goalDescription} ({bonusPoints} points, Done {completedTimes}/{targetTimes}, Bonus: {bonusPoints})";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal:{goalName}, {goalDescription}, {points}, {completedTimes}, {targetTimes}, {bonusPoints}";
     }
 }
