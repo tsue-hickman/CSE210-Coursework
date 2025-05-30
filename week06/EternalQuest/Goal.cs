f0da77af53fec6d555e18d8c4754dfdb7eb879df
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-29 20:01:37 -0500

    added the child classes and rest of the main goal class

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -2,3 +2,24 @@ using System;
 
 // This is the main Goal class, its abstract b/c all goals share stuff
 public abstract class Goal
+{
+    protected string goalName; // Name of the goal
+    protected string goalDescription; // Description of the goal
+    protected int points; // Points for the goal
+
+    public Goal(string name, string description, int pointsValue)
+    {
+        goalName = name;
+        goalDescription = description;
+        points = pointsValue;
+    }
+}
+
+
+
+//gotta define these in child classes
+public abstract void RecordEvent();
+    public abstract bool IsComplete();
+
+
+
