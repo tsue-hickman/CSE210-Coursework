0efbc68e0d99147e3aab417016bd19713f340577
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-29 21:23:06 -0500

    fixed the program.cs to initiate the goal manager.

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager goalManager = new GoalManager(); // starts the goalmanager 
+        goalManager.Start();// starts the manager
     }
 }
