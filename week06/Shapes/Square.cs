2066ae2558c7a6c02b4ebff061d9a9228c06d252
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-30 19:50:49 -0500

    went back fixed the side length error and made the replace method

--- week06/Shapes/Square.cs
+++ week06/Shapes/Square.cs
@@ -1,10 +1,15 @@
 public class Square : Shapes
 {
-    private float _sideLength = side; // store side length
+    private float _sideLength; // store side length
 
     public Square(string color, float side) : base(color)
     {
         _sideLength = side; // set side
+    }
+
+    public override float GetArea()
+    {
+        return _sideLength * _sideLength; // area is the square of the side
     }
 
 
