556a16030e74424af987962ca86c9e411fa71734
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-30 20:19:40 -0500

    finished adding the shapes and creating a foreach loop to  go through and print shape info

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -6,10 +6,19 @@ class Program
     static void Main(string[] args)
     {
         // need to create a list for shapes 
-        List<Shapes> shapes = new List<Shapes>();
+        List<Shapes> shapeList = new List<Shapes>();
 
         // here are the shapes from the other cs files
-        shapeList
+        shapeList.Add(new Square("red", 5));
+        shapeList.Add(new Rectangle("blue", 4, 6));
+        shapeList.Add(new Circle("green", 3));
+
+
+        // loops the list and print info out
+        foreach (Shapes s in shapeList)
+        {
+            Console.WriteLine($"Color: {s.GetColor()}, Area: {s.GetArea()}");
+        }
     }
 }
 
