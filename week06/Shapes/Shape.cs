097cb2b2cec5749581df91b09d89b75947c02ad8
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-30 19:25:50 -0500

    added a string to get color

--- week06/Shapes/Shape.cs
+++ week06/Shapes/Shape.cs
@@ -1,4 +1,16 @@
 public abstract class Shapes
 {
     private string _color; //color of the shape
+
+    public Shapes(string shape_color)
+    {
+        _color = shape_color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public
 }
