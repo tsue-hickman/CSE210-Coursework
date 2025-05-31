1b732e9265cf29457bb35089f63037a0209d9543
Tsue_Hic22009 <hic22009@byui.edu>
2025-05-30 19:56:59 -0500

    coded the formula for the circle and about to add the return statement

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,14 @@
+public class Circle : Shape
+{
+    private float rad; // circle radius
+
+    public Circle(string color, float radius) : base(color)
+    {
+        rad = radius; // set the radius
+    }
+
+    public override float GetArea()
+    {
+        return
+    }
+}
